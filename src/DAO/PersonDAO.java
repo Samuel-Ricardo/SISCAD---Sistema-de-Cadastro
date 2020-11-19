e8d6f72c5dc6f4315869d88669372d530ab148e3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-19 15:59:45 -0300

    Created: Function -  public boolean update(Person person) throws UpdateErrorException (PersonDAO)

--- src/DAO/PersonDAO.java
+++ src/DAO/PersonDAO.java
@@ -5,6 +5,11 @@
  */
 package DAO;
 
+import Exception.DeletedRecordException;
+import Exception.ElementNotFoundException;
+import Exception.EmptyDatabaseException;
+import Exception.RegistrationSuccessfullyRegistredException;
+import Exception.UpdateErrorException;
 import JDBC.ConnectionFactory;
 import Model.Interface.PersonInterface;
 import Model.Person;
@@ -12,6 +17,7 @@ import Util.Dialoger;
 import com.mysql.jdbc.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  *
@@ -21,10 +27,12 @@ public class PersonDAO implements PersonInterface{
     
     private Connection connection;
     
-    public boolean insert(Person person) {
+    @Override
+    public boolean inert(Person person) throws RegistrationSuccessfullyRegistredException {
+        
         connect();
         PreparedStatement statement = null;
-        String sql = "INSERT INTO usuario (nome , endereco, cargo, sexo, ativo) VALUES (?, ?, ?, ?, ?);";
+        String sql = "INSERT INTO pessoa (nome , endereco, cargo, sexo, ativo) VALUES (?, ?, ?, ?, ?);";
 
         try {
 
@@ -53,5 +61,51 @@ public class PersonDAO implements PersonInterface{
 
         connection = ConnectionFactory.getConnection();
 
+    }
+
+    @Override
+    public boolean update(Person person) throws UpdateErrorException {
+       
+        connect();
+            PreparedStatement statement = null;
+            String sql = "UPDATE pessoa SET nome = ?, SET endereco = ?, SET cargo = ?, SET  sexo = ?, SET ativo = ?  WHERE id = ?";
+
+            try {
+
+                statement = connection.prepareStatement(sql);   
+
+
+
+                statement.setString(1, person.getName());     
+                statement.setString(2, person.getAddress());
+                statement.setString(3, person.getCargo());
+                statement.setString(4, person.getGenre());
+                statement.setInt(5, person.getAtivo());
+                statement.setInt(6, person.getId());
+
+                statement.execute();    
+
+                return true;    
+            } catch (SQLException ex) {
+                Dialoger.errorMessage(null, "Erro ao Atualizar os dados:", ex);  
+                return false;   
+            } finally {
+                ConnectionFactory.closeConnection(connection, statement);  // closes all connections regardless of success  // fecha todas as conexoes independente de sucesso
+            }
+    }
+
+    @Override
+    public boolean delete(Person person) throws DeletedRecordException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> selectAll() throws EmptyDatabaseException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Person select(Person person) throws ElementNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
