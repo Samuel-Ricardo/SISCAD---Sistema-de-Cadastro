2a1c771890856adb0efe41469b3c0b2eb330cd6c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-19 16:59:49 -0300

    Created: Function -  public Person generatePerson(ResultSet result) (PersonFactory)

--- src/Factory/PersonFactory.java
+++ src/Factory/PersonFactory.java
@@ -5,12 +5,39 @@
  */
 package Factory;
 
+import Model.Person;
+import Util.Dialoger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Samuel
  */
 public class PersonFactory {
     
-    
+    public Person generatePerson(ResultSet result){
+       
+        Person person = null;
+        
+        try {
+            
+            person = new Person();
+            
+            person.setId(result.getInt("id"));
+            person.setName(result.getString("nome"));
+            person.setAddress(result.getString("endereco"));
+            person.setAtivo(result.getInt("ativo"));
+            person.setCargo(result.getString("cargo"));
+            person.setGenre(result.getString("sexo"));
+            
+        } catch (SQLException ex) {
+            Dialoger.errorMessage(null, "Erro ao gerar uma pessoa", ex);
+        }
+        
+        return person;
+    }
     
 }
