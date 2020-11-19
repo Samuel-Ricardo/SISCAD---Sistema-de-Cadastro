6349a632f0b3f2c523f6e5b4e51c09543b422db3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-19 15:41:12 -0300

    Created: Function - public List<Person> selectAll () throws EmptyDatabaseException; (PersonInterface)

--- src/Model/Interface/PersonInterface.java
+++ src/Model/Interface/PersonInterface.java
@@ -7,6 +7,7 @@ package Model.Interface;
 
 import Exception.*;
 import Model.Person;
+import java.util.List;
 
 /**
  *
@@ -19,4 +20,6 @@ public interface PersonInterface {
     public void update (Person person) throws UpdateErrorException;
     
     public void delete (Person person) throws DeletedRecordException;
+    
+    public List<Person> selectAll () throws EmptyDatabaseException;
 }
