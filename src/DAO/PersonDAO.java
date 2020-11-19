97de654effdc42ac7a61c6482c482301cf22a89d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-19 15:46:07 -0300

    Created: Function - private void connect() (PersonDAO)

--- src/DAO/PersonDAO.java
+++ src/DAO/PersonDAO.java
@@ -5,10 +5,20 @@
  */
 package DAO;
 
+import JDBC.ConnectionFactory;
+import com.mysql.jdbc.Connection;
+
 /**
  *
  * @author Samuel
  */
 public class PersonDAO {
     
+    private Connection connection;
+    
+    private void connect() {
+
+        connection = ConnectionFactory.getConnection();
+
+    }
 }
