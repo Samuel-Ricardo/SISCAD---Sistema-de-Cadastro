92902e0c2c8047f86055c9b322827325368b8cb1
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-19 11:11:15 -0300

    Created: Class - public class EmptyDatabaseException extends Exception (Exceptions)

---
+++ src/Exception/EmptyDatabaseException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exception;
+
+/**
+ *
+ * @author Samuel
+ */
+public class EmptyDatabaseException extends Exception{
+
+    public EmptyDatabaseException() {
+        super("Nenhum registro cadastrado na Base de Dados");
+    }
+    
+    @Override
+    public String getMessage(){
+        
+        return "Nenhum registro cadastrado na Base de Dados";
+    }
+    
+    @Override
+    public String toString(){
+     
+        return "Nenhum registro cadastrado na Base de Dados";
+    } 
+}
