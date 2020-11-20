dfe76170d7bf947186df6860decdde63894f789a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-20 14:35:51 -0300

    Created: Class - public class UpdaterController (Controller/Maintenance)

---
+++ src/Controller/Maintenance/UpdaterController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.Maintenance;
+
+import DAO.PersonDAO;
+import Exception.RegistrationSuccessfullyRegistredException;
+import Exception.UpdateErrorException;
+import Helper.Helper;
+import Model.Person;
+import Util.Dialoger;
+import View.Maintenance.UpdaterView;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ButtonGroup;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+
+/**
+ *
+ * @author Samuel
+ */
+public class UpdaterController {
+    
+    private final UpdaterView view;
+    private final Helper helper;
+    private final PersonDAO dao;
+    
+    
+
+    public UpdaterController(UpdaterView view) {
+    
+        this.view = view;
+        helper = new Helper(view);
+        dao = new PersonDAO();
+
+    }
+
+    public void ClearFields() {
+       
+        helper.clearFields();
+    }
+
+    public void Updater() {
+    
+        if(helper.isFieldsEmpty() || view.getJLabelId().getText().equals("")){
+            
+            Dialoger.message(null, "Algum campo está vazio!!!!!"
+                    + "\n"
+                    + "\n Por Favor Certifique-se de que todos os campos estao preenchidos");
+        }else{
+            
+            try {
+                Person person = helper.getPerson();
+                person.setId(view.getJLabelId());
+                
+                if(dao.update(person)){
+                    Dialoger.message(view, "Atualizado com Sucesso");
+                }
+            } catch (UpdateErrorException ex) {
+                Logger.getLogger(UpdaterController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    
+    }
+    
+}
