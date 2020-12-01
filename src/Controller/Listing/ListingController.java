0592a62f708c3816eac48ac137354cec968b1d29
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-01 17:11:16 -0300

    Create: Class - public class ListingController  (Controller/Listing)

---
+++ src/Controller/Listing/ListingController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.Listing;
+
+import View.Listing.Listing;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ListingController {
+    
+    private Listing view;
+
+    public ListingController(Listing view) {
+        this.view = view;
+    }
+    
+    
+}
